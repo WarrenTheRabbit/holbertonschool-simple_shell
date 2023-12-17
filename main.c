99387075fe656e67622ffbf978b92e80bde7af70
root <rabbitsinwarrens@gmail.com>
2023-12-16 16:54:59 -0800

    refactor: use switch for branching

--- main.c
+++ main.c
@@ -14,22 +14,26 @@ int main(void)
 		command = readline(stream, input_buffer);
 		status = evaluate(command);
 		
-		if (status == EXIT_COMMAND)
+		switch (status)
 		{
-			close_input_buffer(input_buffer);
-			exit(EXIT_SUCCESS);
-		}
-		else if (status == COMMAND_NOT_FOUND)
-		{
-			print_command_not_found_error(command);
-		}
-		else if (status == ENV_COMMAND)
-		{
-			printenv_with_environ();
-		}
-		else
-		{
-			printf("ERROR - unhandled case");
+			case EXIT_COMMAND:
+				close_input_buffer(input_buffer);
+				exit(EXIT_SUCCESS);
+				break;
+
+			case COMMAND_NOT_FOUND:
+				print_command_not_found_error(command);
+				break;
+
+			case ENV_COMMAND:
+				printenv_with_environ();
+				break;
+
+			default:
+				printf("unhandled case\n");
+				break;
 		}
 	}
+	
+	return (EXIT_FAILURE);
 }
