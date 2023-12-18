098e900461f5f1d0061555ae317449d01037e8bd
tingru0515 <7687@holbertonstudents.com>
2023-12-17 21:22:01 -0800

    fixing trim funtion

--- string_utilities.c
+++ string_utilities.c
@@ -3,8 +3,11 @@
 void trim(char * const a)
 {
     char *p = a, *q = a;
-    while (isspace(*q))            ++q;
-    while (*q)                     *p++ = *q++;
+    while (isspace(*q))
+	    ++q;
+    while (*q) 
+	    *p++ = *q++;
     *p = '\0';
-    while (p > a && isspace(*--p)) *p = '\0';
+    while (p > a && isspace(*--p)) 
+	    *p = '\0';
 }
