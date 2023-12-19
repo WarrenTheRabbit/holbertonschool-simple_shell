d26413d7fa2dec9bdde10f913fc2c1d48b5968e8
root <rabbitsinwarrens@gmail.com>
2023-12-18 16:36:40 -0800

    chore: remove trim debugging statements

--- scanner.c
+++ scanner.c
@@ -4,7 +4,6 @@ void initialise_command_array(char *line, char *args[], int max_args)
 {
 	int arg_count = 0;
 	char *token;
-	int i;
 
 	if (line == NULL)
 		return;
@@ -17,15 +16,6 @@ void initialise_command_array(char *line, char *args[], int max_args)
 		token = strtok(NULL, " ");
 	}
 	
-	for (i = 0; i < arg_count; i++)
-	{
-		if (args[i])
-		{
-			printf("args[%d] = %s\n", i, args[i]);
-			trim(args[i]);
-			printf("trim(args[%d]) = %s\n", i, args[i]);
-		}
-	}
 	/* NULL terminate array for exec call. */
 	args[arg_count] = NULL;
 }
