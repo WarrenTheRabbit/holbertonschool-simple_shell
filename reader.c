0b35b020c30346391f3fafd70af217eba45ea48b
tingru0515 <7687@holbertonstudents.com>
2023-12-18 20:12:06 -0800

    betty style reader.c

--- reader.c
+++ reader.c
@@ -22,6 +22,7 @@
 InputBuffer *new_input_buffer(void)
 {
 	InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
+
 	input_buffer->buffer = NULL;
 	input_buffer->buffer_length = 0;
 	input_buffer->input_length = 0;
@@ -39,7 +40,7 @@ InputBuffer *new_input_buffer(void)
 char *readline(FILE *stream, InputBuffer *input_buffer)
 {
 	ssize_t bytes_read = getline(
-					&(input_buffer->buffer), 
+					&(input_buffer->buffer),
 					&(input_buffer->buffer_length),
 					stream
 			);
