e9f4a654afe5c0b5d4ca46eaf4cf78cf172f8de3
tingru0515 <7687@holbertonstudents.com>
2023-12-18 19:12:33 -0800

    move the function of is_executable out to the file is_executable.c

---
+++ is_executable.c
@@ -0,0 +1,13 @@
+int is_executable(char *str)
+{
+	struct stat file_stat;
+
+	if (stat(str, &file_stat) < 0)
+		return (0);
+	if (!S_ISREG(file_stat.st_mode))
+		return (0);
+	if (file_stat.st_mode & S_IXUSR)
+		return (1);
+
+	return (0);
+}
