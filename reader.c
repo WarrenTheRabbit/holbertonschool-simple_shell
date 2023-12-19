bc43c9295511c6000cc89939732e4ee773e3fc9c
tingru0515 <7687@holbertonstudents.com>
2023-12-18 20:08:39 -0800

    betty style reader.c

--- reader.c
+++ reader.c
@@ -2,7 +2,7 @@
 
 
 /**
- * new_input_buffer - returns a small wrapper around the state needed by 
+ * new_input_buffer - returns a small wrapper around the state needed by
  * getline()
  *
  * Description:
@@ -10,12 +10,12 @@
  * call.
  *
  * getline(char **lineptr, size_t *n, FILE *stream) reads up to an including
- * the newline character. Unlike 'gets' and 'fgets', it does a read that is safe
- * and complete: it uses realloc to resize the lineptr buffer to ensure there
- * is enough room to store the complete line. It won't trunate the read or 
- * access invalid memory. If the buffer length is longer than the input, getline
- * will use the existing buffer space and null terminate the string, leaving the
- * buffer beyond it untouched.
+ * the newline character. Unlike 'gets' and 'fgets', it does a read that
+ * is safe and complete: it uses realloc to resize the lineptr buffer to
+ * ensure there is enough room to store the complete line. It won't trunate
+ * the read or access invalid memory. If the buffer length is longer than
+ * the input, getline will use the existing buffer space and null terminate
+ * the string, leaving the buffer beyond it untouched.
  *
  * Return: an InputBuffer object
  */
@@ -31,7 +31,8 @@ InputBuffer *new_input_buffer(void)
 
 
 /**
- * readline - reads a line of input into a buffer and excludes the newline character
+ * readline - reads a line of input into a buffer and excludes the
+ * newline character
  * @input_buffer - a wrapper for the state needed by getline()
  * Return: nothing
  */
@@ -56,7 +57,7 @@ char *readline(FILE *stream, InputBuffer *input_buffer)
 
 /**
  * close_input_buffer - frees the memory allocated by new_input_buffer()
- * 	and readline()
+ * and readline()
  * @input_buffer: a struct that stores the state needed by getline()
  * Return: nothing
  */
