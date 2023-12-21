7a0d40ebe3e7bc281510b4f5b813da332a6fbc2e
root <rabbitsinwarrens@gmail.com>
2023-12-21 01:42:22 -0800

    refactor: add Command type
    This is the second step in creating a more object-oriented shell.
    Previously, an Arguments object was introduced. It is now used in
    the call that processes commands.
    
    The next phase is to encapsulate program execution logic into a
    Command structure. One of its members will be the previously
    introduced Arguments object.

---
+++ command.h
@@ -0,0 +1,14 @@
+#ifndef ARGUMENTS_H
+#define ARGUMENTS_H
+#include "arguments.h"
+#endif
+
+#include <stdlib.h>
+#include <stddef.h>
+
+typedef struct Command
+{
+	char *command_name
+	Arguments *args;
+	int (*execute)(struct Command *self);
+} Command;
