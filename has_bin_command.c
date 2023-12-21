c876ec1ce720766781e203db6ab3ed40d39a13a8
tingru0515 <7687@holbertonstudents.com>
2023-12-21 14:10:20 -0800

    add blank line & delete trailing whitespace

--- has_bin_command.c
+++ has_bin_command.c
@@ -18,10 +18,11 @@ int has_bin_command(char *str)
 	char *PATH, cp_path[1024];
 	struct dirent *entry;
 	DIR *dir_pointer;
+
 	PATH = getenv(key);
 	strcpy(cp_path, PATH);
 	token = strtok(cp_path, delimiter);
-	dir_pointer = opendir(command_path);	
+	dir_pointer = opendir(command_path);
 	while (token != NULL)
 	{
 		if (strcmp(token, command_path) == 0)
