5ea2b7fa43e90684e8449fb1ae85feba08f7bd5d
root <rabbitsinwarrens@gmail.com>
2023-12-20 19:50:56 -0800

    docs: add docstring to InputBuffer

--- inputbuffer.h
+++ inputbuffer.h
@@ -1,5 +1,19 @@
 #include <stdlib.h>
 
+/**
+ * struct InputBuffer - a wrapper around the state needed for getline()
+ * and its return value
+ * Description:
+ * getline(char **lineptr, size_t *n, FILE *stream) has three state
+ * requirements: - reads an entire line
+ * from `stream` and stores the address of the buffer containing the text
+ * into *lineptr.
+ * If *lineptr is set to NULL before the call, then getline() will allocate
+ * a buffer for storing the line.
+ * @buffer: a string buffer
+ * @buffer_length: variable
+ * @input_length: to store the number of bytes read by getline()
+ */
 typedef struct
 {
         char *buffer;
